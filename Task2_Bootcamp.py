5a47963eec8dfac47226df30255fb1dfd86d889f
HARDIK PATEL <79811743+hrdyq@users.noreply.github.com>
2021-07-05 20:20:16 +0530

    Update Task2_Bootcamp.py

--- Task2_Bootcamp.py
+++ Task2_Bootcamp.py
@@ -1,9 +1,9 @@
 import hashlib
-
 StringInput = input("Enter any string : ")
 OutputA=hashlib.md5(StringInput.encode())
 OutputB=hashlib.sha1(StringInput.encode())
 OutputC=hashlib.sha224(StringInput.encode())
+
 print("the MD5 Hash of the string {} is {}".format(StringInput, OutputA.hexdigest()))
 print("the SHA-1 of the string {} is {}".format(StringInput, OutputB.hexdigest()))
 print("the SHA-224 of the string {} is {}".format(StringInput, OutputC.hexdigest()))
